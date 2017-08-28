055afac3fe5fdbc48f30de48ced7c97369958d20
bacarlino <bacarlino@gmail.com>
2017-08-27 20:55:22 -0400

    Begin Contact and Services pages

--- core/urls.py
+++ core/urls.py
@@ -3,5 +3,11 @@ from django.views.generic import TemplateView
 from . import views
 
 urlpatterns = [
-    url(r'^$', TemplateView.as_view(template_name='core/home.html'), name='test')
+    url(r'^$', TemplateView.as_view(template_name='core/home.html'), name='home'),
+    url(r'^services/$', TemplateView.as_view(template_name='core/services.html'), name='services'),
+    url(r'^projects/$', TemplateView.as_view(template_name='core/projects.html'), name='projects'),
+    url(r'^about/$', TemplateView.as_view(template_name='core/about.html'), name='about'),
+    url(r'^contact/$', TemplateView.as_view(template_name='core/contact.html'), name='contact'),
+
+
 ]
